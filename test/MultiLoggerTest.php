aa59f2971ea230d000e78453ae0e1df170c12aac
Jesse Donat <donatj@gmail.com>
2023-01-31 00:40:37 -0600

    Adds MultiLoggerTest

---
+++ test/MultiLoggerTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Corpus\Loggers;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LogLevel;
+
+class MultiLoggerTest extends TestCase {
+
+	public const ALL_LEVELS = [
+		LogLevel::EMERGENCY,
+		LogLevel::ALERT,
+		LogLevel::CRITICAL,
+		LogLevel::ERROR,
+		LogLevel::WARNING,
+		LogLevel::NOTICE,
+		LogLevel::INFO,
+		LogLevel::DEBUG,
+	];
+
+	public function test_MultiLogger_single() : void {
+		$memoryLogger = new MemoryLogger;
+		$logger       = new MultiLogger($memoryLogger);
+
+		$i   = 0;
+		$log = [];
+		foreach( self::ALL_LEVELS as $level ) {
+			$i++;
+			$logger->log($level, 'test ' . $i, [ 'level' => $level ]);
+			$log[] = MemoryLogger::makeLogRecord($level, 'test ' . $i, [ 'level' => $level ]);
+		}
+
+		$this->assertSame($log, $memoryLogger->getLogs());
+	}
+
+	public function test_MultiLogger_multiple() : void {
+		$memoryLogger1 = new MemoryLogger;
+		$memoryLogger2 = new MemoryLogger;
+		$logger        = new MultiLogger($memoryLogger1, $memoryLogger2);
+
+		$i   = 0;
+		$log = [];
+		foreach( self::ALL_LEVELS as $level ) {
+			$i++;
+			$logger->log($level, 'test ' . $i, [ 'level' => $level ]);
+			$log[] = MemoryLogger::makeLogRecord($level, 'test ' . $i, [ 'level' => $level ]);
+		}
+
+		$this->assertSame($log, $memoryLogger1->getLogs());
+		$this->assertSame($log, $memoryLogger2->getLogs());
+	}
+
+	public function test_MultiLogger_withAdditionalLoggers() : void {
+		$memoryLogger1 = new MemoryLogger;
+		$memoryLogger2 = new MemoryLogger;
+		$logger        = new MultiLogger($memoryLogger1, $memoryLogger2);
+
+		$i   = 0;
+		$log = [];
+		foreach( self::ALL_LEVELS as $level ) {
+			$i++;
+			$logger->log($level, 'test ' . $i, [ 'level' => $level ]);
+			$log[] = MemoryLogger::makeLogRecord($level, 'test ' . $i, [ 'level' => $level ]);
+		}
+
+		$this->assertSame($log, $memoryLogger1->getLogs());
+		$this->assertSame($log, $memoryLogger2->getLogs());
+
+		$memoryLogger3 = new MemoryLogger;
+		$memoryLogger4 = new MemoryLogger;
+		$logger = $logger->withAdditionalLoggers($memoryLogger3, $memoryLogger4);
+
+		$log2 = [];
+		foreach( self::ALL_LEVELS as $level ) {
+			$i++;
+			$logger->log($level, 'test ' . $i, [ 'level' => $level ]);
+			$log2[] = MemoryLogger::makeLogRecord($level, 'test ' . $i, [ 'level' => $level ]);
+		}
+
+		$mergedLogs = array_merge($log, $log2);
+		$this->assertSame($mergedLogs, $memoryLogger1->getLogs());
+		$this->assertSame($mergedLogs, $memoryLogger2->getLogs());
+
+		$this->assertSame($log2, $memoryLogger3->getLogs());
+		$this->assertSame($log2, $memoryLogger4->getLogs());
+	}
+
+}
